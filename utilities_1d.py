4e48771c6491e0eaa3f452f10836fc574f7efb97
Paschalis Bizopoulos <pbizopoulos@iti.gr>
2020-06-20 13:13:55 +0300

    fix images

--- utilities_1d.py
+++ utilities_1d.py
@@ -9,6 +9,8 @@ import wfdb
 
 from torch.utils.data import Dataset
 
+from sparsely_activated_networks_pytorch import _conv1d_same_padding
+
 
 def identity_1d(x, kernel_size):
     return x
@@ -27,85 +29,86 @@ def save_images_1d(model, dataset_name, data, xlim_weights, device, path_results
     plt.close()
 
     model.eval()
-    reconstructed, activations_list = model(data.unsqueeze(0).unsqueeze(0).to(device))
-    similarity_list = torch.zeros_like(activations_list)
-    reconstructions = torch.zeros_like(activations_list)
-    for index_weights, (weights, similarity, activations, reconstruction) in enumerate(zip(model.weights_list, similarity_list[0, :, 0], activations_list[0, :, 0], reconstructions[0, :, 0])):
-        fig, ax = plt.subplots(constrained_layout=True, figsize=(2, 2.2))
-        ax.tick_params(labelbottom=False, labelleft=False)
-        plt.grid(True)
-        plt.autoscale(enable=True, axis='x', tight=True)
-        plt.plot(weights.flip(0).cpu().detach().numpy().T, 'r')
-        plt.xlim([0, xlim_weights])
-        if dataset_name == 'apnea-ecg':
-            if model.sparse_activation.__name__ == 'identity_1d':
-                sparse_activation_name = 'Identity'
-            elif model.sparse_activation.__name__ == 'relu_1d':
-                sparse_activation_name = 'ReLU'
-            elif model.sparse_activation.__name__ == 'topk_absolutes_1d':
-                sparse_activation_name = 'top-k absolutes'
-            elif model.sparse_activation.__name__ == 'extrema_pool_indices_1d':
-                sparse_activation_name = 'Extrema-Pool idx'
-            elif model.sparse_activation.__name__ == 'extrema_1d':
-                sparse_activation_name = 'Extrema'
-            plt.ylabel(sparse_activation_name, fontsize=20)
-        if model.sparse_activation.__name__ == 'relu_1d':
-            plt.title(dataset_name, fontsize=20)
-        plt.savefig(f'{path_results}/{dataset_name}_{model.sparse_activation.__name__}_{len(model.weights_list)}_kernel_{index_weights}.pdf')
-        plt.close()
+    with torch.no_grad():
+        reconstructed, activations_list = model(data.unsqueeze(0).unsqueeze(0).to(device))
+        for index_weights, (weights, activations) in enumerate(zip(model.weights_list, activations_list[0, :, 0])):
+            fig, ax = plt.subplots(constrained_layout=True, figsize=(2, 2.2))
+            ax.tick_params(labelbottom=False, labelleft=False)
+            plt.grid(True)
+            plt.autoscale(enable=True, axis='x', tight=True)
+            plt.plot(weights.flip(0).cpu().detach().numpy().T, 'r')
+            plt.xlim([0, xlim_weights])
+            if dataset_name == 'apnea-ecg':
+                if model.sparse_activation.__name__ == 'identity_1d':
+                    sparse_activation_name = 'Identity'
+                elif model.sparse_activation.__name__ == 'relu_1d':
+                    sparse_activation_name = 'ReLU'
+                elif model.sparse_activation.__name__ == 'topk_absolutes_1d':
+                    sparse_activation_name = 'top-k absolutes'
+                elif model.sparse_activation.__name__ == 'extrema_pool_indices_1d':
+                    sparse_activation_name = 'Extrema-Pool idx'
+                elif model.sparse_activation.__name__ == 'extrema_1d':
+                    sparse_activation_name = 'Extrema'
+                plt.ylabel(sparse_activation_name, fontsize=20)
+            if model.sparse_activation.__name__ == 'relu_1d':
+                plt.title(dataset_name, fontsize=20)
+            plt.savefig(f'{path_results}/{dataset_name}_{model.sparse_activation.__name__}_{len(model.weights_list)}_kernel_{index_weights}.pdf')
+            plt.close()
+
+            similarity = _conv1d_same_padding(data.unsqueeze(0).unsqueeze(0), weights)[0, 0]
+            fig, ax = plt.subplots(constrained_layout=True)
+            ax.tick_params(labelbottom=False, labelleft=False)
+            plt.grid(True)
+            plt.autoscale(enable=True, axis='x', tight=True)
+            plt.plot(similarity.cpu().detach().numpy(), 'g')
+            plt.savefig(f'{path_results}/{dataset_name}_{model.sparse_activation.__name__}_{len(model.weights_list)}_similarity_{index_weights}.pdf')
+            plt.close()
+
+            fig, ax = plt.subplots(constrained_layout=True)
+            ax.tick_params(labelbottom=False, labelleft=False)
+            plt.grid(True)
+            plt.autoscale(enable=True, axis='x', tight=True)
+            p = activations.nonzero()[:, 0]
+            plt.plot(similarity.cpu().detach().numpy(), 'g', alpha=0.5)
+            if p.shape[0] != 0:
+                plt.stem(p.cpu().detach().numpy(), activations[p.cpu().detach().numpy()].cpu().detach().numpy(), 'c', basefmt=' ', use_line_collection=True)
+            plt.savefig(f'{path_results}/{dataset_name}_{model.sparse_activation.__name__}_{len(model.weights_list)}_activations_{index_weights}.pdf')
+            plt.close()
+
+            reconstruction = _conv1d_same_padding(activations.unsqueeze(0).unsqueeze(0), weights)[0, 0]
+            fig, ax = plt.subplots(constrained_layout=True)
+            ax.tick_params(labelbottom=False, labelleft=False)
+            plt.grid(True)
+            plt.autoscale(enable=True, axis='x', tight=True)
+            reconstruction = reconstruction.cpu().detach().numpy()
+            left = p - weights.shape[0]/2
+            right = p + weights.shape[0]/2
+            if weights.shape[0] % 2 == 1:
+                right += 1
+            step = np.zeros_like(reconstruction, dtype=np.bool)
+            left[left < 0] = 0
+            right[right > reconstruction.shape[0]] = reconstruction.shape[0]
+            for l, r in zip(left, right):
+                step[int(l):int(r)] = True
+            pos_signal = reconstruction.copy()
+            neg_signal = reconstruction.copy()
+            pos_signal[step] = np.nan
+            neg_signal[~step] = np.nan
+            plt.plot(pos_signal)
+            plt.plot(neg_signal, color='r')
+            plt.ylim([data.min(), data.max()])
+            plt.savefig(f'{path_results}/{dataset_name}_{model.sparse_activation.__name__}_{len(model.weights_list)}_reconstruction_{index_weights}.pdf')
+            plt.close()
 
         fig, ax = plt.subplots(constrained_layout=True)
         ax.tick_params(labelbottom=False, labelleft=False)
         plt.grid(True)
         plt.autoscale(enable=True, axis='x', tight=True)
-        plt.plot(similarity.cpu().detach().numpy(), 'g')
-        plt.savefig(f'{path_results}/{dataset_name}_{model.sparse_activation.__name__}_{len(model.weights_list)}_similarity_{index_weights}.pdf')
-        plt.close()
-
-        fig, ax = plt.subplots(constrained_layout=True)
-        ax.tick_params(labelbottom=False, labelleft=False)
-        plt.grid(True)
-        plt.autoscale(enable=True, axis='x', tight=True)
-        p = activations.nonzero()[:, 0]
-        plt.plot(similarity.cpu().detach().numpy(), 'g', alpha=0.5)
-        if p.shape[0] != 0:
-            plt.stem(p.cpu().detach().numpy(), activations[p.cpu().detach().numpy()].cpu().detach().numpy(), 'c', basefmt=' ', use_line_collection=True)
-        plt.savefig(f'{path_results}/{dataset_name}_{model.sparse_activation.__name__}_{len(model.weights_list)}_activations_{index_weights}.pdf')
-        plt.close()
-
-        fig, ax = plt.subplots(constrained_layout=True)
-        ax.tick_params(labelbottom=False, labelleft=False)
-        plt.grid(True)
-        plt.autoscale(enable=True, axis='x', tight=True)
-        reconstruction = reconstruction.cpu().detach().numpy()
-        left = p - weights.shape[0]/2
-        right = p + weights.shape[0]/2
-        if weights.shape[0] % 2 == 1:
-            right += 1
-        step = np.zeros_like(reconstruction, dtype=np.bool)
-        left[left < 0] = 0
-        right[right > reconstruction.shape[0]] = reconstruction.shape[0]
-        for l, r in zip(left, right):
-            step[int(l):int(r)] = True
-        pos_signal = reconstruction.copy()
-        neg_signal = reconstruction.copy()
-        pos_signal[step] = np.nan
-        neg_signal[~step] = np.nan
-        plt.plot(pos_signal)
-        plt.plot(neg_signal, color='r')
+        plt.plot(data.cpu().detach().numpy(), alpha=0.5)
+        plt.plot(reconstructed[0, 0].cpu().detach().numpy(), 'r')
         plt.ylim([data.min(), data.max()])
-        plt.savefig(f'{path_results}/{dataset_name}_{model.sparse_activation.__name__}_{len(model.weights_list)}_reconstruction_{index_weights}.pdf')
+        plt.savefig(f'{path_results}/{dataset_name}_{model.sparse_activation.__name__}_{len(model.weights_list)}_reconstructed.pdf')
         plt.close()
-
-    fig, ax = plt.subplots(constrained_layout=True)
-    ax.tick_params(labelbottom=False, labelleft=False)
-    plt.grid(True)
-    plt.autoscale(enable=True, axis='x', tight=True)
-    plt.plot(data.cpu().detach().numpy(), alpha=0.5)
-    plt.plot(reconstructed[0, 0].cpu().detach().numpy(), 'r')
-    plt.ylim([data.min(), data.max()])
-    plt.savefig(f'{path_results}/{dataset_name}_{model.sparse_activation.__name__}_{len(model.weights_list)}_reconstructed.pdf')
-    plt.close()
 
 def download_physionet(dataset_name_list, path_cache):
     for dataset_name in dataset_name_list:
