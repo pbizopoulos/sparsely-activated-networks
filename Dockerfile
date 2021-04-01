74b5f01bc2a949e8695a21850799a887d7636d93
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2021-04-02 01:54:37 +0300

    merge custom packages (safs, sans)

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM pytorch/pytorch:1.8.1-cuda11.1-cudnn8-runtime
-RUN apt-get update && apt-get install -y git
 COPY requirements.txt .
 RUN python -m pip install --no-cache-dir --upgrade pip && python -m pip install --no-cache-dir -r requirements.txt
 COPY . .
--- main.py
+++ main.py
@@ -19,13 +19,11 @@ from torch.utils.data import DataLoader, Dataset
 from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision import datasets, transforms
 
-from sparse_activation_functions_pytorch import TopKAbsolutes1D, ExtremaPoolIndices1D, Extrema1D, TopKAbsolutes2D, ExtremaPoolIndices2D, Extrema2D
-from sparsely_activated_networks_pytorch import SAN1d, SAN2d, _conv1d_same_padding, _conv2d_same_padding
-
 plt.rcParams['font.size'] = 20
 plt.rcParams['image.interpolation'] = 'none'
 plt.rcParams['savefig.format'] = 'pdf'
 plt.rcParams['savefig.bbox'] = 'tight'
+
 
 class identity_2d(nn.Module):
     def __init__(self, k):
@@ -41,6 +39,101 @@ class relu_2d(nn.Module):
     def forward(self, input):
         return F.relu(input)
 
+def topk_absolutes_2d(x, k):
+    x_flattened = x.view(x.shape[0], -1)
+    primary_extrema = torch.zeros_like(x_flattened)
+    topk, extrema_indices = torch.topk(abs(x_flattened), k)
+    return primary_extrema.scatter(-1, extrema_indices, x_flattened.gather(-1, extrema_indices)).view(x.shape)
+
+def extrema_pool_indices_2d(x, kernel_size):
+    x_flattened = x.view(x.shape[0], -1)
+    primary_extrema = torch.zeros_like(x_flattened)
+    output, extrema_indices = F.max_pool2d_with_indices(abs(x), kernel_size)
+    return primary_extrema.scatter(-1, extrema_indices[..., 0, 0], x_flattened.gather(-1, extrema_indices[..., 0, 0])).view(x.shape)
+
+def extrema_2d(x, minimum_extrema_distance):
+    primary_extrema = torch.zeros_like(x)
+    dx = x[:, :, :, 1:] - x[:, :, :, :-1]
+    dy = x[:, :, 1:, :] - x[:, :, :-1, :]
+    dx_padright_greater = F.pad(dx, [0, 1, 0, 0]) > 0
+    dx_padleft_less = F.pad(dx, [1, 0, 0, 0]) <= 0
+    dy_padright_greater = F.pad(dy, [0, 0, 0, 1]) > 0
+    dy_padleft_less = F.pad(dy, [0, 0, 1, 0]) <= 0
+    sign = (1 - torch.sign(x)).bool()
+    valleys_x = dx_padright_greater & dx_padleft_less & sign
+    valleys_y = dy_padright_greater & dy_padleft_less & sign
+    peaks_x = ~dx_padright_greater & ~dx_padleft_less & ~sign
+    peaks_y = ~dy_padright_greater & ~dy_padleft_less & ~sign
+    peaks = peaks_x & peaks_y
+    valleys = valleys_x & valleys_y
+    extrema = peaks | valleys
+    extrema.squeeze_(1)
+    for index, (x_, e_) in enumerate(zip(x, extrema)):
+        extrema_indices = torch.nonzero(e_, as_tuple=False)
+        extrema_indices_indices = torch.argsort(abs(x_[0, e_]), 0, True)
+        extrema_indices_sorted = extrema_indices[extrema_indices_indices]
+        is_secondary_extrema = torch.zeros_like(extrema_indices_indices, dtype=torch.bool)
+        for i, (extrema_index_x, extrema_index_y) in enumerate(extrema_indices_sorted):
+            if not is_secondary_extrema[i]:
+                extrema_indices_r = extrema_indices_sorted[:, 0] >= extrema_index_x - minimum_extrema_distance[0]
+                extrema_indices_l = extrema_indices_sorted[:, 0] <= extrema_index_x + minimum_extrema_distance[0]
+                extrema_indices_t = extrema_indices_sorted[:, 1] >= extrema_index_y - minimum_extrema_distance[1]
+                extrema_indices_b = extrema_indices_sorted[:, 1] <= extrema_index_y + minimum_extrema_distance[1]
+                extrema_indices_m = extrema_indices_r & extrema_indices_l & extrema_indices_t & extrema_indices_b
+                is_secondary_extrema = is_secondary_extrema | extrema_indices_m
+                is_secondary_extrema[i] = False
+        primary_extrema_indices = extrema_indices_sorted[~is_secondary_extrema]
+        for primary_extrema_index in primary_extrema_indices:
+            primary_extrema[index, :, primary_extrema_index[0], primary_extrema_index[1]] = x_[0, primary_extrema_index[0], primary_extrema_index[1]]
+    return primary_extrema
+
+class TopKAbsolutes2D(nn.Module):
+    def __init__(self, k):
+        super().__init__()
+        self.k = k
+
+    def forward(self, input):
+        return topk_absolutes_2d(input, self.k)
+
+class ExtremaPoolIndices2D(nn.Module):
+    def __init__(self, k):
+        super().__init__()
+        self.k = k
+
+    def forward(self, input):
+        return extrema_pool_indices_2d(input, self.k)
+
+class Extrema2D(nn.Module):
+    def __init__(self, minimum_extrema_distance):
+        super().__init__()
+        self.minimum_extrema_distance = minimum_extrema_distance
+
+    def forward(self, input):
+        return extrema_2d(input, self.minimum_extrema_distance)
+
+class SAN2d(nn.Module):
+    def __init__(self, sparse_activation_list, kernel_size_list):
+        super(SAN2d, self).__init__()
+        self.sparse_activation_list = nn.ModuleList(sparse_activation_list)
+        self.weights_list = nn.ParameterList([nn.Parameter(0.1*torch.ones(kernel_size, kernel_size)) for kernel_size in kernel_size_list])
+
+    def forward(self, batch_x):
+        activations_list = torch.zeros(batch_x.shape[0], len(self.weights_list), *batch_x.shape[1:], device=batch_x.device)
+        reconstructions_list = torch.zeros(batch_x.shape[0], len(self.weights_list), *batch_x.shape[1:], device=batch_x.device)
+        for index_weights, (sparse_activation, weights) in enumerate(zip(self.sparse_activation_list, self.weights_list)):
+            similarity = _conv2d_same_padding(batch_x, weights)
+            activations_list[:, index_weights] = sparse_activation(similarity)
+            reconstructions_list[:, index_weights] = _conv2d_same_padding(activations_list[:, index_weights], weights)
+        reconstruction = reconstructions_list.sum(1)
+        return reconstruction
+
+def _conv2d_same_padding(x, weights):
+    padding = weights.shape[0] - 1
+    odd = int(padding % 2 != 0)
+    if odd:
+        x = F.pad(x, [0, odd, 0, odd])
+    out = F.conv2d(x, weights.unsqueeze(0).unsqueeze(0), padding=padding//2)
+    return out
 
 def save_images_2d(model, sparse_activation_name, data, dataset_name):
     model = model.to('cpu')
@@ -97,7 +190,6 @@ def save_images_2d(model, sparse_activation_name, data, dataset_name):
         plt.savefig(f'tmp/{dataset_name}-{sparse_activation_name}-{len(model.weights_list)}-reconstructed')
         plt.close()
 
-
 class FNN(nn.Module):
     def __init__(self, sample_data, num_classes):
         super(FNN, self).__init__()
@@ -116,7 +208,6 @@ class Hook():
         self.input = input
         self.output = output
 
-
 class identity_1d(nn.Module):
     def __init__(self, k):
         super().__init__()
@@ -131,6 +222,89 @@ class relu_1d(nn.Module):
     def forward(self, input):
         return F.relu(input)
 
+def topk_absolutes_1d(x, k):
+    primary_extrema = torch.zeros_like(x)
+    topk, extrema_indices = torch.topk(abs(x), k)
+    return primary_extrema.scatter(-1, extrema_indices, x.gather(-1, extrema_indices))
+
+def extrema_pool_indices_1d(x, kernel_size):
+    primary_extrema = torch.zeros_like(x)
+    output, extrema_indices = F.max_pool1d_with_indices(abs(x), kernel_size)
+    return primary_extrema.scatter(-1, extrema_indices, x.gather(-1, extrema_indices))
+
+def extrema_1d(x, minimum_extrema_distance):
+    primary_extrema = torch.zeros_like(x)
+    dx = x[:, :, 1:] - x[:, :, :-1]
+    dx_padright_greater = F.pad(dx, [0, 1]) > 0
+    dx_padleft_less = F.pad(dx, [1, 0]) <= 0
+    sign = (1 - torch.sign(x)).bool()
+    valleys = dx_padright_greater & dx_padleft_less & sign
+    peaks = ~dx_padright_greater & ~dx_padleft_less & ~sign
+    extrema = peaks | valleys
+    extrema.squeeze_(1)
+    for index, (x_, e_) in enumerate(zip(x, extrema)):
+        extrema_indices = torch.nonzero(e_, as_tuple=False)
+        extrema_indices_indices = torch.argsort(abs(x_[0, e_]), 0, True)
+        extrema_indices_sorted = extrema_indices[extrema_indices_indices][:, 0]
+        is_secondary_extrema = torch.zeros_like(extrema_indices_indices, dtype=torch.bool)
+        for i, extrema_index in enumerate(extrema_indices_sorted):
+            if not is_secondary_extrema[i]:
+                extrema_indices_r = extrema_indices_sorted >= extrema_index - minimum_extrema_distance
+                extrema_indices_l = extrema_indices_sorted <= extrema_index + minimum_extrema_distance
+                extrema_indices_m = extrema_indices_r & extrema_indices_l
+                is_secondary_extrema = is_secondary_extrema | extrema_indices_m
+                is_secondary_extrema[i] = False
+        primary_extrema_indices = extrema_indices_sorted[~is_secondary_extrema]
+        primary_extrema[index, :, primary_extrema_indices] = x_[0, primary_extrema_indices]
+    return primary_extrema
+
+class TopKAbsolutes1D(nn.Module):
+    def __init__(self, k):
+        super().__init__()
+        self.k = k
+
+    def forward(self, input):
+        return topk_absolutes_1d(input, self.k)
+
+class ExtremaPoolIndices1D(nn.Module):
+    def __init__(self, k):
+        super().__init__()
+        self.k = k
+
+    def forward(self, input):
+        return extrema_pool_indices_1d(input, self.k)
+
+class Extrema1D(nn.Module):
+    def __init__(self, minimum_extrema_distance):
+        super().__init__()
+        self.minimum_extrema_distance = minimum_extrema_distance
+
+    def forward(self, input):
+        return extrema_1d(input, self.minimum_extrema_distance)
+
+class SAN1d(nn.Module):
+    def __init__(self, sparse_activation_list, kernel_size_list):
+        super(SAN1d, self).__init__()
+        self.sparse_activation_list = nn.ModuleList(sparse_activation_list)
+        self.weights_list = nn.ParameterList([nn.Parameter(0.1*torch.ones(kernel_size)) for kernel_size in kernel_size_list])
+
+    def forward(self, batch_x):
+        activations_list = torch.zeros(batch_x.shape[0], len(self.weights_list), *batch_x.shape[1:], device=batch_x.device)
+        reconstructions_list = torch.zeros(batch_x.shape[0], len(self.weights_list), *batch_x.shape[1:], device=batch_x.device)
+        for index_weights, (sparse_activation, weights) in enumerate(zip(self.sparse_activation_list, self.weights_list)):
+            similarity = _conv1d_same_padding(batch_x, weights)
+            activations_list[:, index_weights] = sparse_activation(similarity)
+            reconstructions_list[:, index_weights] = _conv1d_same_padding(activations_list[:, index_weights], weights)
+        reconstruction = reconstructions_list.sum(1)
+        return reconstruction
+
+def _conv1d_same_padding(x, weights):
+    padding = weights.shape[0] - 1
+    odd = int(padding % 2 != 0)
+    if odd:
+        x = F.pad(x, [0, odd])
+    out = F.conv1d(x, weights.unsqueeze(0).unsqueeze(0), padding=padding//2)
+    return out
 
 def save_images_1d(model, sparse_activation_name, dataset_name, data, xlim_weights):
     model = model.to('cpu')
