70c420305a64d5b4a197fb91a986f21810d99656
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2022-12-08 13:07:50 +0200

    fix

---
+++ docs/sanResize.js
@@ -0,0 +1,114 @@
+const sanResize = {
+	"input": {
+		"data": null,
+		"dataNoise": null,
+		"channelArray": [{
+			"amplitudeBase": 0.5,
+			"amplitudeMax": 0,
+			"amplitudes": null,
+			"data": null,
+			"distanceMax": 20,
+			"distanceMin": 10,
+			"distances": null,
+			"motifSize": 30,
+			"motifType": "cos",
+			"use": true
+		}, {
+			"amplitudeBase": 0.5,
+			"amplitudeMax": 0,
+			"amplitudes": null,
+			"data": null,
+			"distanceMax": 20,
+			"distanceMin": 10,
+			"distances": null,
+			"motifSize": 10,
+			"motifType": "cos",
+			"use": false
+		}, {
+			"amplitudeBase": 0.5,
+			"amplitudeMax": 0,
+			"amplitudes": null,
+			"data": null,
+			"distanceMax": 20,
+			"distanceMin": 10,
+			"distances": null,
+			"motifSize": 10,
+			"motifType": "cos",
+			"use": false
+		}],
+		"noiseInitialize": false,
+		"noiseSigma": 0.1,
+		"noiseTypeKey": "normal",
+		"quantizationStatesNum": 100,
+		"resizeFunctionKey": "bilinear",
+		"resizeMultiplier": 1,
+		"size": 100,
+		"standardize": false,
+		"velocity": 0
+	},
+	"learningRateExponent": -1,
+	"lossFunctionKey": "mse",
+	"neuronArray": [{
+		"activation": {
+			"amplitudeMin": 0,
+			"data": null,
+			"distanceMin": 8,
+			"function_": "extrema",
+			"regulated": false,
+			"regulates": false
+		},
+		"convEncoderUse": true,
+		"kernel": {
+			"amplitude": 1,
+			"initialization": "constant",
+			"resizeFunction": "bilinear",
+			"resizeMultiplier": 3,
+			"size": 3,
+			"stride": 1,
+			"strideResizeFunction": "bilinear"
+		},
+		"use": true
+	}, {
+		"activation": {
+			"amplitudeMin": 0,
+			"data": null,
+			"distanceMin": 0,
+			"function_": "extrema",
+			"regulated": false,
+			"regulates": false
+		},
+		"convEncoderUse": false,
+		"kernel": {
+			"amplitude": 1,
+			"initialization": "constant",
+			"resizeFunction": "bilinear",
+			"resizeMultiplier": 1,
+			"size": 1,
+			"stride": 1,
+			"strideResizeFunction": "bilinear"
+		},
+		"use": false
+	}, {
+		"activation": {
+			"amplitudeMin": 0,
+			"data": null,
+			"distanceMin": 0,
+			"function_": "extrema",
+			"regulated": false,
+			"regulates": false
+		},
+		"convEncoderUse": false,
+		"kernel": {
+			"amplitude": 1,
+			"initialization": "constant",
+			"resizeFunction": "bilinear",
+			"resizeMultiplier": 1,
+			"size": 1,
+			"stride": 1,
+			"strideResizeFunction": "bilinear"
+		},
+		"use": false
+	}],
+	"optimizerKey": "sgd",
+	"referenceFunction": "absolute topk"
+};
