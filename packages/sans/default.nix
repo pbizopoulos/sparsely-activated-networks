6074d6d0809b55887ed1e2850ca6e4a45a38d587
Paschalis Bizopoulos <pbizop@gmail.com>
2026-09-15 00:40:41 +0300

    .

--- packages/sans/default.nix
+++ packages/sans/default.nix
@@ -23,7 +23,10 @@ python.pkgs.buildPythonPackage {
     mainProgram = pname;
   };
   nativeBuildInputs = nativeDeps;
-  passthru.python = python;
+  passthru = {
+    inherit python;
+    canonicalization.tests = [ ];
+  };
   propagatedBuildInputs = pythonDeps;
   pyproject = false;
   src = ./.;
