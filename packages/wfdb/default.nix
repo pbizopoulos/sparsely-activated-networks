cadeec6603d92509503a7ee7b88fa0820e5b99ba
Paschalis Bizopoulos <pbizop@gmail.com>
2026-09-21 19:54:31 +0300

    .

--- packages/wfdb/default.nix
+++ packages/wfdb/default.nix
@@ -16,13 +16,17 @@ in
 python.pkgs.buildPythonPackage rec {
   format = "wheel";
   pname = "wfdb";
+  postInstall = ''
+    substituteInPlace "$out/${python.sitePackages}/wfdb/io/annotation.py" \
+      --replace-fail '.values, inplace=True' '.to_numpy(), inplace=True'
+  '';
   propagatedBuildInputs = pythonDeps;
   pythonImportsCheck = [ pname ];
   src = python.pkgs.fetchPypi rec {
     inherit pname version format;
     dist = python;
     python = "py3";
-    sha256 = "u9nSkbwgOLBYZhb82Acs/ckGrHDBhYsVEzeMQSmgEQ8=";
+    sha256 = "qhgBz4NXl7kFGreVX7kAzmxfHhts2cyZeebHME4VcGM=";
   };
-  version = "4.3.0";
+  version = "4.3.1";
 }
